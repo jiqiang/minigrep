09313fb34cd7073d3799e2dbda2f7883bb416a28
Glenn Ji <qiang.ji@yahoo.com.au>
2018-03-07 21:21:58 +1100

    Gouping configuration values

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,23 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
-    println!("{}", query);
+    println!("{}", config.query);
     println!("{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config {query, filename}
 }
