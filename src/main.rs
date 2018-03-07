4c9c3b8cee63a6dc24e1e75a06e1eb1d0046dc28
Glenn Ji <qiang.ji@yahoo.com.au>
2018-03-07 21:03:26 +1100

    Extracting the argument parser

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //let query = &args[1];
-    let filename = &args[2];
-    
+    let (query, filename) = parse_config(&args);
+
     let mut f = File::open(filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
-    
+
+    println!("{}", query);
     println!("{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
 }
