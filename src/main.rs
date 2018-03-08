02d324faf6e1bc2442227d17fc1db4c2870d6388
Glenn Ji <glenn.ji@nab.com.au>
2018-03-08 14:13:48 +1100

    Handle errors returned from run() in main

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,10 @@ fn main() {
     
     println!("{}", config.query);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
 fn run(config: Config) -> Result<(), Box<Error>> {
